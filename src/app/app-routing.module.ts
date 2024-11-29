9a24d649e81952a569a17a3330e3d9caeefe93a7
Lissette Teneguzñay <lilianatene2003@gmail.com>
2024-11-28 21:56:33 -0500

    examenfgh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+const routes: Routes = [ 
+  { 
+    path: 'inicio', 
+    loadChildren: () => import('./pages/inicio/inicio.module').then(m => 
+m.InicioPageModule) 
+  }, 
+
+  { 
+    path: '', 
+    redirectTo: 'inicio', 
+    pathMatch: 'full' 
+  }, 
+ 
 ];
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+@NgModule({ 
+  imports: [ 
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules 
+}) 
+  ], 
+  exports: [RouterModule] 
+}) 
+export class AppRoutingModule { } 
